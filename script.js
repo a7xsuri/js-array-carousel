98acbfc45a6a65dcdb09f2bbabd24b266264912f
a7xsuri <leonardofilipposurico@gmail.com>
2022-03-30 19:07:10 +0200

    finish

--- script.js
+++ script.js
@@ -21,5 +21,34 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+let currentIndex = 0;
+const slideArray = document.getElementsByClassName("slide");
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+console.log(currentIndex)
+console.log(slideArray[0])
+up.addEventListener("click", function () {
+    if (currentIndex == 0) {
+        slideArray[currentIndex].classList.remove("active");
+        currentIndex = 4
+        slideArray[currentIndex].classList.add("active")
+    }
+    else {
+        slideArray[currentIndex].classList.remove("active");
+        currentIndex = currentIndex - 1;
+        slideArray[currentIndex].classList.add("active")
+    }
+})
 
-const 
+down.addEventListener("click", function () {
+    if (currentIndex == 4) {
+        slideArray[currentIndex].classList.remove("active");
+        currentIndex = 0
+        slideArray[currentIndex].classList.add("active")
+    }
+    else {
+        slideArray[currentIndex].classList.remove("active");
+        currentIndex += 1;
+        slideArray[currentIndex].classList.add("active")
+    }
+})
